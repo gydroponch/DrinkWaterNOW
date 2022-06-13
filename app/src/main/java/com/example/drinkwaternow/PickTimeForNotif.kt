e0271519b724c6b6a25125dccbcf9022aa790e00
gydroponch <keerinsta@gmail.com>
2022-06-13 20:55:33 +0700

    notifications finally work

--- app/src/main/java/com/example/drinkwaternow/PickTimeForNotif.kt
+++ app/src/main/java/com/example/drinkwaternow/PickTimeForNotif.kt
@@ -6,6 +6,7 @@ import android.app.PendingIntent
 import android.app.TimePickerDialog
 import android.content.Context
 import android.content.Intent
+import android.os.Build
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
@@ -27,42 +28,12 @@ class PickTimeForNotif: AppCompatActivity(), TimePickerFragment.OnCompleteListen
 
     private var fromHour = 9
     private var fromMinute = 0
-    private var toHour = 0
+    private var toHour = 23
     private var toMinute = 0
 
     private var intervalHour = 99
     private var intervalMinute = 99
     private var intervalMinutes = 60
-
-    private fun saveTimeToInternalStorage(prefName1: String, prefName2: String, Hour:Int, Minute:Int) {
-        val sharedPref = this.getSharedPreferences(APP_PREFERENCES, MODE_PRIVATE)
-        with (sharedPref.edit()){
-            putInt(prefName1, Hour)
-            putInt(prefName2, Minute)
-            apply()
-        }
-    }
-
-    private fun saveIntervalToInternalStorage(interval:Int){
-        val sharedPref = this.getSharedPreferences(APP_PREFERENCES, MODE_PRIVATE)
-        with (sharedPref.edit()){
-            putInt(INTERVAL, interval)
-            apply()
-        }
-    }
-
-    private fun loadTimeFromInternalStorage() {
-        val sharedPref = this.getSharedPreferences(APP_PREFERENCES, MODE_PRIVATE)
-            fromHour = sharedPref.getInt(SAVED_FROM_HOUR, 9)
-            fromMinute = sharedPref.getInt(SAVED_FROM_MINUTE, 0)
-            toHour = sharedPref.getInt(SAVED_TO_HOUR, 0)
-            toMinute = sharedPref.getInt(SAVED_TO_MINUTE, 0)
-    }
-
-    private fun loadIntervalFromInternalStorage() {
-        val sharedPref = this.getSharedPreferences(APP_PREFERENCES, MODE_PRIVATE)
-        intervalMinutes = sharedPref.getInt(INTERVAL, 60)
-    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -93,7 +64,7 @@ class PickTimeForNotif: AppCompatActivity(), TimePickerFragment.OnCompleteListen
                     }
                     intervalMinutes = (intervalHour*60 + intervalMinute)
                     intervalTextView!!.text = String.format(Locale.getDefault(), "уведомления через каждые %02d минут", intervalMinutes)
-                    saveIntervalToInternalStorage(intervalMinutes)
+                    saveTimeIntervalToInternalStorage(intervalMinutes)
                 }
 
             val style = THEME_HOLO_LIGHT
@@ -104,7 +75,7 @@ class PickTimeForNotif: AppCompatActivity(), TimePickerFragment.OnCompleteListen
         }
 
         loadTimeFromInternalStorage()
-        loadIntervalFromInternalStorage()
+        loadTimeIntervalFromInternalStorage()
 
         intervalTextView.text="уведомления через каждые $intervalMinutes минут"
         fromTimeEditText.setText("$fromHour : $fromMinute")
@@ -141,6 +112,8 @@ class PickTimeForNotif: AppCompatActivity(), TimePickerFragment.OnCompleteListen
             val fromTimeEditText = findViewById<EditText>(R.id.editTextFromTime)
             fromTimeEditText.setText("$fromHour : $fromMinute")
             saveTimeToInternalStorage(SAVED_FROM_HOUR, SAVED_FROM_MINUTE, fromHour, fromMinute)
+            //распределение уведомлений
+            scheduleNotification(toHour, toMinute)
         }
         else if (fragNumber==1) {
             toHour = calendar.get(Calendar.HOUR_OF_DAY)
@@ -148,8 +121,39 @@ class PickTimeForNotif: AppCompatActivity(), TimePickerFragment.OnCompleteListen
             val fromTimeEditText2 = findViewById<EditText>(R.id.editTextToTime)
             fromTimeEditText2.setText("$toHour : $toMinute")
             saveTimeToInternalStorage(SAVED_TO_HOUR, SAVED_TO_MINUTE, toHour, toMinute)
+            //распределение уведомлений
             scheduleNotification(toHour, toMinute)
         }
+    }
+
+    private fun saveTimeToInternalStorage(prefName1: String, prefName2: String, Hour:Int, Minute:Int) {
+        val sharedPref = this.getSharedPreferences(APP_PREFERENCES, MODE_PRIVATE)
+        with (sharedPref.edit()){
+            putInt(prefName1, Hour)
+            putInt(prefName2, Minute)
+            apply()
+        }
+    }
+
+    private fun saveTimeIntervalToInternalStorage(interval:Int){
+        val sharedPref = this.getSharedPreferences(APP_PREFERENCES, MODE_PRIVATE)
+        with (sharedPref.edit()){
+            putInt(INTERVAL, interval)
+            apply()
+        }
+    }
+
+    private fun loadTimeFromInternalStorage() {
+        val sharedPref = this.getSharedPreferences(APP_PREFERENCES, MODE_PRIVATE)
+        fromHour = sharedPref.getInt(SAVED_FROM_HOUR, 9)
+        fromMinute = sharedPref.getInt(SAVED_FROM_MINUTE, 0)
+        toHour = sharedPref.getInt(SAVED_TO_HOUR, 23)
+        toMinute = sharedPref.getInt(SAVED_TO_MINUTE, 0)
+    }
+
+    private fun loadTimeIntervalFromInternalStorage() {
+        val sharedPref = this.getSharedPreferences(APP_PREFERENCES, MODE_PRIVATE)
+        intervalMinutes = sharedPref.getInt(INTERVAL, 60)
     }
 
     private fun scheduleNotification(toHour:Int, toMinute:Int){
@@ -159,23 +163,48 @@ class PickTimeForNotif: AppCompatActivity(), TimePickerFragment.OnCompleteListen
         intent.putExtra(titleExtra,textTitle)
         intent.putExtra(messageExtra, textContent)
 
-        val pendingIntent=PendingIntent.getBroadcast(
-            applicationContext,
-            notificationID,
-            intent,
-            PendingIntent.FLAG_IMMUTABLE or PendingIntent.FLAG_UPDATE_CURRENT
-        )
-
         val calendar = Calendar.getInstance()
+        val currentYear = calendar.get(Calendar.YEAR)
+        val currentMonth = calendar.get(Calendar.MONTH)
+        val currentDay = calendar.get(Calendar.DAY_OF_MONTH)
+        calendar.set(Calendar.YEAR, currentYear)
+        calendar.set(Calendar.MONTH, currentMonth)
+        calendar.set(Calendar.DAY_OF_MONTH, currentDay)
         calendar.set(Calendar.SECOND, 0)
         calendar.set(Calendar.MILLISECOND, 0)
-        calendar.set(Calendar.HOUR_OF_DAY, toHour)
-        calendar.set(Calendar.MINUTE, toMinute)
-
         val alarmManager = getSystemService(Context.ALARM_SERVICE) as AlarmManager
-        val time = calendar.timeInMillis
-        val alarmClockInfo = AlarmManager.AlarmClockInfo(time, pendingIntent)
-        alarmManager.setAlarmClock(alarmClockInfo,pendingIntent)
+
+        var iNotificationHour = fromHour
+        var iNotificationMinute = toMinute
+        val notificationsCount = toHour - fromHour
+        //РАСПРЕДЕЛЕНИЕ НАПОМИНАНИЙ
+        //TODO Сделать нормальное распределение напоминаний
+        for (i in 0..notificationsCount){
+
+            val pendingIntent=PendingIntent.getBroadcast(
+                applicationContext,
+                i,
+                intent,
+                PendingIntent.FLAG_IMMUTABLE or PendingIntent.FLAG_UPDATE_CURRENT
+            )
+            if (iNotificationHour*60+toMinute >= calendar.get(Calendar.HOUR_OF_DAY)*60+toMinute) {
+                calendar.set(Calendar.HOUR_OF_DAY, iNotificationHour)
+                calendar.set(Calendar.MINUTE, toMinute)
+                val time = calendar.timeInMillis
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                    alarmManager.setExactAndAllowWhileIdle(
+                        AlarmManager.RTC_WAKEUP,
+                        time,
+                        pendingIntent
+                    )
+                }
+                println("_________i: " + i)
+                println("iHour: " + iNotificationHour)
+                println("iMinute: " + iNotificationMinute)
+                println("millis: " + time)
+            }
+            iNotificationHour++
+        }
 
     }
 }
